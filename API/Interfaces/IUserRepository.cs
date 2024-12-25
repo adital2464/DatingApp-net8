11a9652632a33fb8b207c3a7c316fd180db1ccdc
adi <adital2464@gmail.com>
2024-12-25 13:53:12 +0200

    End of section 13

--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -1,6 +1,7 @@
 using System;
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using Microsoft.EntityFrameworkCore.Storage;
 
 namespace API.Interfaces;
@@ -12,6 +13,6 @@ public interface IUserRepository
     Task<IEnumerable<AppUser>> GetUserAsync();
     Task<AppUser?> GetUserByIdAsync(int id );
     Task<AppUser?> GetUserByUsernameAsync(string username);
-    Task<IEnumerable<MemberDto>> GetMembersAsync();
+    Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams);
     Task<MemberDto?> GetMemberAsync (string username);
 }
